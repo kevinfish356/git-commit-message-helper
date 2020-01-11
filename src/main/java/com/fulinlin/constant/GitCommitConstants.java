54eb617be59134c95a31752f39fed8ae0f4c4095
Yu👽 <kaiwenyu@amazon.com>
2020-01-10 16:18:24 -0800

    Changed default template, removed unused gif and pickture

--- src/main/java/com/fulinlin/constant/GitCommitConstants.java
+++ src/main/java/com/fulinlin/constant/GitCommitConstants.java
@@ -6,12 +6,10 @@ package com.fulinlin.constant;
  * @create: 2019-12-08 11:37
  **/
 public class GitCommitConstants {
-    public static final String DEFAULT_TEMPLATE = "#if($type)${type}#end#if($scope)(${scope})#end: #if($subject)${subject}#end\n" +
-            "${newline}\n" +
-            "#if($body)${body}#end\n" +
-            "${newline}\n" +
-            "#if($changes)BREAKING CHANGE: ${changes}#end\n" +
-            "${newline}\n" +
-            "#if($closes)Closes ${closes}#end\n";
+    public static final String DEFAULT_TEMPLATE = "#if($type)${type}#end #if($subject)${subject}#end\n"
+            + "${newline}\n"
+            + "#if($body)${body}#end\n"
+            + "${newline}\n"
+            + "#if($closes)Closes ${closes}#end\n";
 
 }
